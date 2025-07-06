3a6cc363d0dea85b0efc4db9f8431b5a7d5dd679
Mathiasheartman <mathiasokpe86@gmail.com>
2025-07-06 17:31:01 +0100

    Initial commit

--- client/src/pages/owner/ManageCars.jsx
+++ client/src/pages/owner/ManageCars.jsx
@@ -1,9 +1,72 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { dummyCarData } from "../../assets/assets";
+import Title from "../../components/owner/Title";
 
 function ManageCars() {
+  const currency = import.meta.env.VITE_CURRENCY;
+
   const [cars, setCars] = useState();
 
-  return <div></div>;
+  const fetchOwnerCars = async () => {
+    setCars(dummyCarData);
+  };
+
+  useEffect(() => {
+    fetchOwnerCars();
+  }, []);
+
+  return (
+    <div className="px-4 pt-10 md:px-10 w-full">
+      <Title
+        title="Manage Cars"
+        subTitle="View all listed cars, update their details, or remove them from the booking platform."
+      />
+
+      <div className="max-w-3xl w-full rounded-md overflow-hidden border border-borderColor mt-6">
+        <table className="w-full border-collapse text-left text-sm text-gray-600">
+          <thead className="text-gray-500">
+            <tr>
+              <th className="p-3 font-medium">Car</th>
+              <th className="p-3 font-medium max-md:hidden">Category</th>
+              <th className="p-3 font-medium">Price</th>
+              <th className="p-3 font-medium max-md:hidden">Status</th>
+              <th className="p-3 font-medium">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cars?.map((car, index) => (
+              <tr key={index} className="border-t border-borderColor">
+                <td className="p-3 flex items-center gap-3">
+                  <img
+                    src={car.image}
+                    alt=""
+                    className="h-12 w-12 aspect-square rounded-md object-cover"
+                  />
+                  <div className="max-md:hidden">
+                    <p className="font-medium">
+                      {car.brand} {car.model}
+                    </p>
+                    <p className="text-xs text-gray-500">
+                      {car.seating_capacity} ◾ {car.transmission}
+                    </p>
+                  </div>
+                </td>
+
+                <td className="p-3 max-md:hidden">{car.category}</td>
+                <td className="p-3">
+                  {currency}
+                  {car.pricePerDay}/day
+                </td>
+                <td className="p-3 max-md:hidden">
+                  <span>{car.isAvailable ? "Available" : "Unavailable"}</span>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default ManageCars;
